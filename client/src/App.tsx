5fa6c25189fc4e16e260754d23e449d5ded28019
Joyal George K J <joyalgeorgekj@gmail.com>
2025-12-16 18:48:27 +0530

    Created a working frontend with proper socket

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState, type FormEvent } from "react";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3000");
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [messages, setMessages] = useState<
+        | {
+              id: string;
+              text: any;
+              sender: string;
+              time: number;
+          }[]
+        | []
+    >([]);
+    const [text, setText] = useState<string>("");
+    const messagesEndRef = useRef<null | HTMLDivElement>(null);
+    const inputRef = useRef<null | HTMLInputElement>(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        socket.on("connect", () => {
+            console.log("Connected:", socket.id);
+        });
+
+        socket.on("chat:message", (msg) => {
+            setMessages((prev) => [...prev, msg]);
+        }); 
+
+        if(inputRef.current) inputRef.current.focus();
+
+        return () => {
+            socket.off("chat:message");
+            setMessages([]);
+        };
+    }, []);
+
+    // auto-scroll to bottom
+    useEffect(() => {
+        if (messagesEndRef)
+            messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, [messages]);
+
+    const sendMessage = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!text.trim()) return;
+
+        socket.emit("chat:message", { text });
+        setText("");
+    };
+
+    return (
+        <div className="grid min-h-screen">
+            <div className="flex flex-col p-2.5 gap-2 w-fit mx-auto">
+                {messages.map((m) => (
+                    <>
+                        <div
+                            key={m.id}
+                            className="flex flex-col gap-1 w-full max-w-[320px]"
+                        >
+                            <div className="flex items-center space-x-1.5 rtl:space-x-reverse">
+                                <span className="text-sm font-semibold text-heading">
+                                    {socket.id === m.sender
+                                        ? "You"
+                                        : m.sender.slice(0, 5)}
+                                    :
+                                </span>
+                                <span className="text-sm text-body">
+                                    {new Date(m.time).getHours() +
+                                        ":" +
+                                        new Date(m.time).getMinutes()}
+                                </span>
+                            </div>
+                            <div className="flex flex-col px-4 py-3 rounded-md bg-blue-600/75 text-white font-bold w-fit cursor-pointer">
+                                <p className="text-sm text-body"> {m.text}</p>
+                            </div>
+                        </div>
+                    </>
+                ))}
+                <div ref={messagesEndRef} />
+                <form onSubmit={sendMessage} className="flex h-fit gap-2">
+                    <input
+                        value={text}
+                        type="text"
+                        name="message-input"
+                        className="px-4 py-2 outline-0 rounded-md border border-neutral-700/50"
+                        onChange={(e) => setText(e.target.value)}
+                        placeholder="Type a message..."
+                        autoFocus
+                        ref={inputRef}
+                    />
+                    <button className="px-4 py-2 outline-0 rounded-md border border-neutral-200/50 bg-blue-600/75 text-white cursor-pointer">
+                        Send
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
